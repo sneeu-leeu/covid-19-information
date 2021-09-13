7e4c39c8fcb684ac34e4da698d432b141c96a38c
sneeu-leeu <sa070197@gmail.com>
2021-09-13 15:04:58 +0200

    Render and display data from API

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import Home from './components/Home';
 
 function App() {
   return (
-      <>
-        <header>
-          <h3 className="m-0">COVID-19 STATS</h3>
-        </header>
-        <main>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </main>
-      </>
+    <>
+      <header>
+        <h3 className="m-0">COVID-19 STATS</h3>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import { fetchData } from './redux/CovidAPI/covidAPI';
 import App from './App';
+
+store.dispatch(fetchData);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
