8cc2a926d8260f99f3ca98c8c8fa74813db171cd
sneeu-leeu <sa070197@gmail.com>
2021-09-14 17:10:36 +0200

    Add styling

--- src/redux/CovidInfo/covidInfo.test.js
+++ src/redux/CovidInfo/covidInfo.test.js
@@ -1,4 +1,4 @@
-import reducer, { addCountry } from './covid';
+import reducer, { addCountry } from './covidInfo';
 
 test('should return the initial state', () => {
   expect(reducer(undefined, {})).toEqual(
